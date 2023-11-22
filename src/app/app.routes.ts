3673114e8586925eaea343fc562d1e9536fc2504
Nishanth-es <nishanthe_mca22-24@macfast.ac.in>
2023-11-22 22:44:56 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',component: HomePageComponent
+    }
+
+];
